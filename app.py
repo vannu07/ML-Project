c41e10414c0c7d3e4ad496d5b7617fff4cc06a27
vannu07 <varnitkumar1234@gmail.com>
2025-06-29 22:39:16 +0530

    git untrack

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ if __name__=="__main__":
         data_ingestion=DataIngestion()
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
-        data_transformation_config=DataTransformationConfig()
-        data_transformation=DataTransformation()
-        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        #data_transformation_config=DataTransformationConfig()
+        #data_transformation=DataTransformation()
+        #train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
 
         ## Model Training
 
-        model_trainer=ModelTrainer()
-        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        #model_trainer=ModelTrainer()
+        #print(model_trainer.initiate_model_trainer(train_arr,test_arr))
         
     except Exception as e:
         logging.info("Custom Exception")
