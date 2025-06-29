35aa9331b8a50b5cd0750badd75609eb2adc7757
vannu07 <varnitkumar1234@gmail.com>
2025-06-29 23:14:31 +0530

    Data Transformation

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ if __name__=="__main__":
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
         #data_transformation_config=DataTransformationConfig()
-        #data_transformation=DataTransformation()
-        #train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        data_transformation=DataTransformation()
+        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
 
         ## Model Training
 
--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -23,6 +23,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         try:
             ##reading the data from mysql
+            #df=read_sql_data()
             df=pd.read_csv(os.path.join('notebook/data','raw.csv'))
             logging.info("Reading completed mysql database")
 
