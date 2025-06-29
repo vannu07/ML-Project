088ebee05fe684614f4e6a534b4474170e7a0d23
vannu07 <varnitkumar1234@gmail.com>
2025-06-29 18:44:31 +0530

    update

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='krish',
-author_email='krishnaik06@gmail.com',
+author='Varnit Kumar',
+author_email='kumar.varnit.16@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 )
